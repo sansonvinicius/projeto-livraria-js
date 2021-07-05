aed028ac87a31b4e82eb54f4678a468213f6a8f6
sansonvinicius <sansonvinicius@gmail.com>
2021-07-05 17:29:07 -0300

    Inserindo seleção de categoria no app.js

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+//Importar os livtos do arquivo database.js
 const livros = require("./database");
+//Verificar um input para o usuário
+const readline = require('readline-sync')
 
-console.log(livros)
+//Verifica se o usuário quer buscar um livro e qual categoria caso a resposta seja S 
+const entradaInicial = readline.question('Deseja buscar um livro? (S/N)')
+
+if(entradaInicial.toLocaleUpperCase()==='S'){
+    console.log('As categorias disponíveis são:')
+    console.log( 'Produtividade e estilo de vida','/História brasileira',
+    '/Américas', '/Tecnologia', '/Estilo de vida')
+
+    const entradaCategoria = readline.question('Qual categoria está buscando?')
+
+    // busca todos os livros com a mesma categoria selecionada pelo usuário na database.js
+    const retorno = livros.filter(livro => livro.categoria === entradaCategoria)
+
+    console.table(retorno);
+}
